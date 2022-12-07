9f2d9e3ca0e86b6b5057d6af02e5ea0603566b22
Sebastian <sebastian.abramowski@onet.pl>
2022-12-08 00:24:35 +0100

    Added -.png, changed byte_arrays.py - everything done, improved oled1306.py - still needs some changes

--- oled1306.py
+++ oled1306.py
@@ -3,6 +3,7 @@ from ssd1306 import SSD1306_I2C
 import framebuf
 import byte_arrays
 
+
 """
 How to connect:
 VCC - 3V3
@@ -14,19 +15,22 @@ SDA - D21 e.g.
 oled_width = 128
 oled_height = 64
 
-
-
 def oled1306_configuration(scl_pin=22, sda_pin=21):
     i2c = SoftI2C(scl=Pin(scl_pin), sda=Pin(sda_pin))
     return SSD1306_I2C(oled_width, oled_height, i2c)
 
 oled = oled1306_configuration()
 
-fb = framebuf.FrameBuffer(byte_arrays.buffer_5, 32, 64, framebuf.MONO_HLSB)
+def oled1306_print_result(oled_passed: "SSD1306_I2C", buffer1, buffer2):
+    fb = framebuf.FrameBuffer(buffer1, 32, 64, framebuf.MONO_HLSB)
+    fb2 = framebuf.FrameBuffer(buffer2, 32, 64, framebuf.MONO_HLSB)
+    fb3 = framebuf.FrameBuffer(byte_arrays.buffer_, 64, 64, framebuf.MONO_HLSB)
 
-fb2 = framebuf.FrameBuffer(byte_arrays.buffer_3, 32, 64, framebuf.MONO_HLSB)
+    oled_passed.framebuf.blit(fb, 0, 0)
+    oled_passed.framebuf.blit(fb2, 96, 0)
+    oled_passed.framebuf.blit(fb3, 32, 0)
 
-oled.framebuf.blit(fb, 0, 0)
-oled.framebuf.blit(fb2, 96, 0)
+    oled_passed.invert(1)
+    oled_passed.show()
 
-oled.show()
+oled1306_print_result(oled, byte_arrays.buffer_7, byte_arrays.buffer_7)
